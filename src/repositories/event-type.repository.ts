f6a4286bdf922c2cf15983d6a3c08547673fd702
RajeshSR0809 <srrajesh.svpur@gmail.com>
2026-06-27 15:57:24 +0000

    feat: adding doesExistBySlug

--- src/repositories/event-type.repository.ts
+++ src/repositories/event-type.repository.ts
@@ -1,6 +1,14 @@
 import { prisma } from "../config/database.js";
 import { CreateEventTypeDto, UpdateEventTypeDto } from "../dtos/event-type.dto.js";
 
+
+async function  doesExistsBySlug(){
+    const doesExists = await findByHostAndSlug(1, "");
+
+    return doesExists
+}
+
+
 export async function findByHostId(hostId: number) {
     const eventTypes = await prisma.eventType.findMany({
         where: {
@@ -55,6 +63,8 @@ export async function findByHostAndSlug(hostId: number, slug: string) {
     });
     return eventType;
 }
+
+
 
 
 // /blogs/:id
